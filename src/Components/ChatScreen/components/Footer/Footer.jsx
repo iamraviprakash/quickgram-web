040cc028dee39fd87f3fa631078f5c089cbbe4ae
Ravi Prakash <raviprakash.coder@gmail.com>
2021-12-28 21:03:13 +0530

    feat: add footer component in chat screen

---
+++ src/Components/ChatScreen/components/Footer/Footer.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Footer = (props) => {
+  const { sendMessage } = props;
+  const [message, setMessage] = useState('');
+
+  return (
+    <div className="flex py-4 px-48 mb-8 gap-2">
+      <input
+        type="text"
+        placeholder="Message"
+        className="flex grow rounded-lg p-4 focus:outline-none"
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button
+        className="bg-neutral-500 text-white px-4 py-2 rounded-full"
+        onClick={() => sendMessage({ message })}
+      >
+        Send
+      </button>
+    </div>
+  );
+};
+
+Footer.propTypes = {
+  sendMessage: PropTypes.func,
+};
+
+export default Footer;
