504b72d7ea7342e816d5b5c258d0973ef5870ec8
iamraviprakash <raviprakash.coder@gmail.com>
2022-05-17 10:10:13 +0530

    refactor: replace Avatar with baseui Avatar

--- src/Components/ChatScreen/components/Header/Header.jsx
+++ src/Components/ChatScreen/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Avatar from '../../../Avatar';
+import { Avatar } from 'baseui/avatar';
 
 const Header = ({ title }) => {
   return (
     <div className="p-4 bg-white grid grid-cols-[min-content_1fr] border-b border-neutral-200 gap-x-4">
-      <Avatar label={title} />
+      <Avatar name={title} size={'scale1200'} />
       <div className="grid">
         <div className="font-bold">{title}</div>
         <div className="text-neutral-600">{'last seen 5m ago'}</div>
--- src/Components/Sidebar/Components/SidebarItem/SidebarItem.jsx
+++ src/Components/Sidebar/Components/SidebarItem/SidebarItem.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Avatar from '../../../Avatar';
+import { Avatar } from 'baseui/avatar';
 import { BsThreeDotsVertical } from 'react-icons/bs';
 
 const SidebarItem = ({ item, onItemClick }) => {
   return (
     <div className="grid p-4 rounded-lg grid-cols-[1fr_16px] hover:bg-neutral-100 items-center">
       <div className="grid gap-x-4 grid-cols-[min-content_1fr]">
-        <Avatar label={title} />
+        <Avatar name={item.title} size={'scale1200'} />
         <div className="grid grid-flow-row auto-rows-min">
           <div className="font-bold text-ellipsis whitespace-nowrap overflow-hidden">
             {item.title}
