0c3582c689bf94e3703f0300823c781931aefabc
iamraviprakash <raviprakash.coder@gmail.com>
2022-08-14 20:47:41 +0530

    fix: undefined useEffect error

--- src/Components/RoomSetup/Components/CreateRoom/CreateRoom.jsx
+++ src/Components/RoomSetup/Components/CreateRoom/CreateRoom.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Input } from 'baseui/input';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
