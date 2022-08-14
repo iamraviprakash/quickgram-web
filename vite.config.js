1f3fd10bb77e246918fdd33833946bba51bdaf2e
iamraviprakash <raviprakash.coder@gmail.com>
2022-08-14 15:27:57 +0530

    fix: global is not defined in react vite
    Issue source: https://github.com/vitejs/vite/discussions/5912

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,10 @@ export default defineConfig({
   resolve: {
     alias,
   },
+  define: {
+    // By default, Vite doesn't include shims for NodeJS/
+    // necessary for segment analytics lib to work
+    global: {},
+  },
   plugins: [react()],
 });
