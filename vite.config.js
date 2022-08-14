0676ca370e9beec215ceaf91fdbffd2c52980799
iamraviprakash <raviprakash.coder@gmail.com>
2022-08-14 18:28:20 +0530

    feat: add support to read PORT from env

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { alias } from './alias';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias,
-  },
-  define: {
-    // By default, Vite doesn't include shims for NodeJS/
-    // necessary for segment analytics lib to work
-    global: {},
-  },
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
+      port: env.PORT || 3000,
+    },
+    resolve: {
+      alias,
+    },
+    define: {
+      // By default, Vite doesn't include shims for NodeJS/
+      // necessary for segment analytics lib to work
+      global: {},
+    },
+    plugins: [react()],
+  };
 });
