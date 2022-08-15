3ae8a0f9e4f40f9fcc13a5d252ffa5c21179ebd2
iamraviprakash <raviprakash.coder@gmail.com>
2022-08-15 13:15:04 +0530

    chore: update server endpoint

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 // API endpoint
 export const API_ENDPOINT =
-  'https://glitchgram-server.herokuapp.com/graphql';
+  'https://quickgram-server.herokuapp.com/graphql';
 
 export const BASE_URL = 'http://localhost:3000/';
