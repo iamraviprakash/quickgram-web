fa1d367e8f150ac2573db22cf5278815cbfc46f8
iamraviprakash <raviprakash.coder@gmail.com>
2022-11-27 16:07:51 +0530

    chore: add log in alias

--- alias.js
+++ alias.js
@@ -1,5 +1,10 @@
 import path from 'path';
 
+console.log({
+  currentdir: './',
+  dirname: path.resolve(__dirname, 'src'),
+});
+
 export const alias = [
   { find: '@', replacement: path.resolve(__dirname, 'src') },
 ];
