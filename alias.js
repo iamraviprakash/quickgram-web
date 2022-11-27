492027ee1782192eddc967348cdee1250024d0ff
iamraviprakash <raviprakash.coder@gmail.com>
2022-11-27 16:39:47 +0530

    chore: update alias for @ with dirname

--- alias.js
+++ alias.js
@@ -4,8 +4,9 @@ console.log({
   currentdir: './',
   dirname: path.resolve(__dirname, 'src'),
   dirname2: path.resolve('src'),
+  dirname3: path.resolve(__dirname),
 });
 
 export const alias = [
-  { find: '@', replacement: path.resolve('src') },
+  { find: '@', replacement: path.resolve(__dirname) },
 ];
